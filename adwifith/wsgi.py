dd2d718e1995168a146be63459f4fbcc92fd5569
ren096358 <34854876+ren096358@users.noreply.github.com>
2018-05-16 13:38:18 +0800

    * 修改 wsgi

--- adwifith/wsgi.py
+++ adwifith/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adwifith.settings")
 
 application = get_wsgi_application()
+
+sys.path.append("/var/www/html/adwifith_test/adwifith")
