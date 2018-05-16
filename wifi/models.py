853dca7935fd43c5970f245c53c98eb73671fc89
ren096358 <34854876+ren096358@users.noreply.github.com>
2018-05-16 11:10:00 +0800

    * 修改 DB 設置

--- wifi/models.py
+++ wifi/models.py
@@ -4,15 +4,15 @@ import pymysql
 class Model(object):
 
     def __init__(self):
-        # db_host = "103.25.97.10"
-        # db_user = "root"
-        # db_password = "adwifihovering"
-        # db_name = "itt_th"
-
-        db_host = "localhost"
+        db_host = "103.25.97.10"
         db_user = "root"
-        db_password = ""
+        db_password = "adwifihovering"
         db_name = "itt_th"
+
+        # db_host = "localhost"
+        # db_user = "root"
+        # db_password = ""
+        # db_name = "itt_th"
 
         self._db = pymysql.connect(db_host, db_user, db_password, db_name, charset="utf8", port=3306)
         self._cursor = self._db.cursor()
